fb4c7337e1bbedbcbc49e43219e2add141e11389
Abbas Alasgarov <devabbasalasgarov@gmail.com>
2024-07-01 22:00:34 +0400

    Add project files.

---
+++ OnlineWallet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineWallet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
